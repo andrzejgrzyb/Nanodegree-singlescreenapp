e157d5f184754ad97f91d3212958c8772e531529
Andrzej <mgr.inz.andrzej.grzyb@gmail.com>
2017-07-10 21:51:42 +0200

    Initial commit

---
+++ app/src/main/java/pl/com/andrzejgrzyb/SatoGotuje/MainActivity.java
@@ -0,0 +1,13 @@
+package pl.com.andrzejgrzyb.SatoGotuje;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
